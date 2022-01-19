590c32ff25cc4a8c84042b06103c507b3abc8f4d
Max Lopez <soytanmax10@gmail.com>
2022-01-19 12:39:00 +0100

    routguards kicking in

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -7,15 +7,25 @@
 </template>
 
 <script>
+import getUser from '@/composables/getUser'
 import Navbar from '../components/Navbar.vue'
+import { watch } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
 export default {
     components:{
         Navbar
     },
     setup () {
-        
+        const { user } = getUser()
+        const router = useRouter()
 
-        return {}
+        watch(user, () => {
+            if(!user.value){
+                router.push({ name: 'Welcome'})
+            }
+        })
+
+        return { user }
     }
 }
 </script>
